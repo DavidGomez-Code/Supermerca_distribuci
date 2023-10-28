b30c1617ed80ec05c9cec8a23fe2c40ca1fa84a1
bestuser10 <juliangomez190230@gmail.com>
2023-10-27 22:09:38 -0500

    Mi primera carga del archivo scrip_telegram.js del sitio web SUPERMERCADOS Y DISTRIBUCIONES LA ECONOMIA AYA, el cual se encarga de generar la interaccion del icono de la app para que se mantenga en la parte superior de la pagina, independientemente si se va haciendo scroll hacia abajo

---
+++ scrip_telegram.js
@@ -0,0 +1,6 @@
+/*Icono de Telegram*/
+window.onscroll = function() {
+    var bubble = document.querySelector(".float-tele");
+    var scrollY = window.scrollY;
+   bubble.style.top = scrollY + "px";
+  };
