2c2a75696f78c815f34fd7002e6cbfb2351944f0
bestuser10 <juliangomez190230@gmail.com>
2023-11-04 01:20:12 -0500

    Mi segunda carga del archivo style.css del sitio web SUPEMERCADOS Y DISTRIBUCIONES LA ECONOMIA AYA, el cual se le ha añadieron muchas clases de los archivos html, para continuar dandole estilo a las paginas y dejandolas exactamente lo mismo que en el prototipo de figma

--- distribuciones_final.sql
+++ distribuciones_final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-11-2023 a las 04:49:09
+-- Tiempo de generación: 04-11-2023 a las 05:24:45
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
