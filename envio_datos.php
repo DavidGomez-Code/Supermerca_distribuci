a82d2fe7c7c61f63ddcd10970fd1042102c477ff
bestuser10 <juliangomez190230@gmail.com>
2023-11-20 01:05:11 -0500

    Este archivo envio_datos.php a su codigo se le añadieron dos campos mas, los cuales son usuario y contraseña por la razon que se decidio hacer uso de iniciar sesion. La funcion de este codigo es que recibe los datos escritos por el usuario en el formulario del login.php y luego enviarlos por medio del archivo conexion.php a la base de datos.

--- envio_datos.php
+++ envio_datos.php
@@ -1,15 +1,16 @@
 <html>
 <body>
     <link rel="stylesheet" href="style.css">
-    <h1 class="trorte">
+    <h1 class="estilo">
         <?php
         include("conexion.php");
-
         $nombre_completob = $_REQUEST["bnombre_completo"];
         $correo_electronicob = $_REQUEST["bcorreo_electronico"];
         $numero_telefonob = $_REQUEST["bnumero_telefonico"];
         $departamentob = $_REQUEST["bdepartamento"];
         $ciudadb = $_REQUEST["bciudad"];
+        $usuariob = $_REQUEST["buser_complet"];
+        $contraseñab = $_REQUEST["bcontraseña_usu"];
 
         // Generación del código registro
         $Codigo_registro = rand(1, 9999999);
@@ -17,8 +18,7 @@
         // Generación del código cliente
         $Codigo_cliente = rand(1, 9999999);
 
-
-        // Consulta la base de datos para verificar si el nombre ya existe
+        // Se consulta la base de datos para verificar si el nombre ya existe
         $sqlconsultar = "SELECT * FROM registro WHERE LOWER(Nombre_Completo) = LOWER('$nombre_completob')";
         $resultado = mysqli_query($conn, $sqlconsultar);
         if (!$resultado) {
@@ -28,21 +28,20 @@
         if (mysqli_num_rows($resultado) > 0) {
             echo "Ya se ha registrado anteriormente";
             echo "<br><a href='inicio.html'>Regresar al inicio</a>";
-        } else {
+        } 
+        else {
             // Consulta para insertar el nuevo registro
-            $sql = "INSERT INTO registro (Nombre_Completo, Correo_Electrónico, Número_Telefónico, Departamento, Ciudad, Codigo_registro, Codigo_cliente) VALUES ('$nombre_completob', '$correo_electronicob', '$numero_telefonob', '$departamentob', '$ciudadb', '$Codigo_registro', '$Codigo_cliente')";
+            $sql = "INSERT INTO registro (Nombre_Completo, Correo_Electrónico, Número_Telefónico, Departamento, Ciudad, Usuario, Contraseña, Codigo_registro, Codigo_cliente) VALUES ('$nombre_completob', '$correo_electronicob', '$numero_telefonob', '$departamentob', '$ciudadb', '$usuariob', '$contraseñab', '$Codigo_registro', '$Codigo_cliente')";
 
             if (mysqli_query($conn, $sql)) {
                 echo "Registro exitoso";
-                echo "<br><a href='inicio.html'>Regresar al inicio</a>";
+                echo "<br><a href='inicio_sesion.php'>Inicia Sesion ahora</a>";
             } else {
                 echo "Error al enviar los datos: " . mysqli_error($conn);
             }
         }
-
         mysqli_close($conn);
         ?>
-
     </h1>
 </body>
 </html>
